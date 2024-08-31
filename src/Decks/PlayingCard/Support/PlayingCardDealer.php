23b2f346e1b12f9e1d2bf4156ceeae745d1bb9f9
mdram83 <michal.dramowicz@gmail.com>
2024-08-31 20:48:17 +0200

    Cleanup PlayingCardDealer.php comments

--- src/Decks/PlayingCard/Support/PlayingCardDealer.php
+++ src/Decks/PlayingCard/Support/PlayingCardDealer.php
@@ -7,10 +7,6 @@ use MyDramGames\Utils\Decks\PlayingCard\PlayingCardStocksCollection;
 
 interface PlayingCardDealer
 {
-
-    // Not needed, install in dedicated GameServicesProvider to use within specific GamePlay
-    // public function getEmptyStock(bool $unique = true): PlayingCardCollection;
-
     /**
      * Shuffle cards and distribute according to provided definitions.
      * Allows to decide if deck should be shuffled and if requested cards should be dealt by one card or by definition
@@ -28,22 +24,6 @@ interface PlayingCardDealer
         bool $shuffleDeck = true,
         bool $dealOneCardPerDefinition = true,
     ): void;
-
-    // Not needed, added Collection::keys() method instead.
-    // TODO test return types conflicts in first implementation (for this one and below...)
-    // public function getCardsByKeys(PlayingCardCollection $deck, array $keys): PlayingCardCollection;
-
-    // Not needed, added Collection::keys() method instead.
-    // public function getCardsKeys(PlayingCardCollection $stock): array;
-
-    // Not needed, added PlayingCardCollection::sortByKeys(array $keys) method
-    // TODO test return types conflicts in first implementation (for this one and below...)
-    // public function getSortedCards(PlayingCardCollection $stock, array $keys): PlayingCardCollection;
-
-    // Not needed, added Collection::pullFirst() method instead.
-    // TODO test return types conflicts in first implementation (for this one and below...)
-    // public function pullFirstCard(PlayingCardCollection $stock): ?PlayingCard;
-
 
     /**
      * Move cards between the stocks by PlayingCard key
@@ -78,10 +58,4 @@ interface PlayingCardDealer
      * @return PlayingCardCollection
      */
     public function collectCards(PlayingCardCollection $toStock, PlayingCardStocksCollection $fromStocks): PlayingCardCollection;
-
-    // Not needed, utilize PlayingCardCollection::countMatchingKeyCombinations
-    // public function hasStockAnyCombination(PlayingCardCollection $stock, array $combinations): bool;
-
-    // Moved to PlayingCardCollection::countMatchingKeyCombinations
-    // public function countStockMatchingCombinations(PlayingCardCollection $stock, array $combinations): int;
 }
